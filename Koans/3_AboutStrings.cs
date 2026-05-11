dce998aec2e8392262c27bd2919e8c4a94939920
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2026-05-11 21:29:26 +0530

    test 22

--- Koans/3_AboutStrings.cs
+++ Koans/3_AboutStrings.cs
@@ -167,7 +167,7 @@ broken line";
   {
     var world = "World";
     var str = String.Format("Hello, {0}", world);
-    Assert.Equal(FILL_ME_IN, str);
+    Assert.Equal("Hello, World", str);
     // Note that string concatenation and interpolation is more efficient than string.Format
   }
 
